2a411527e7423cbd1427f64da0e83030145e3536
Sentarion <mbalabox@gmail.com>
2021-02-23 22:14:19 +0100

    App test

---
+++ src/test/java/com/creativelabs/scriptscreatorfrontend/ScriptsCreatorFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creativelabs.scriptscreatorfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScriptsCreatorFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
